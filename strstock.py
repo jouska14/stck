9e90c686099234b9a9bd4dd61fb69b5b266004bd
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-14 23:09:46 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -9,7 +9,7 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 df = pd.DataFrame({
-  'first column': [nehal, kopal, molly, mishthi],
+  'first column': ['nehal', 'kopal', 'molly', 'mishthi'],
   'second column': [10, 20, 30, 40]
 })
 
