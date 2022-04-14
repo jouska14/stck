ade7ff9a7698aa7f12940269f895b4cf80289cb6
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-14 23:07:26 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -1,35 +1,16 @@
 import streamlit as st
 import yfinance as yf
 
-def get_ticker(name):
-    company = yf.Ticker(name)
-    return company
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
 
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
 
-st.title("Build and Deploy Stock Market App Using Streamlit")
-st.header("A Basic Data Science Web Application")
-st.sidebar.header("Geeksforgeeks \n TrueGeeks")
-
-company1 = get_ticker("AAPL")
-company2 = get_ticker("GOOGL")
-
-apple = yf.download("AAPL", start="2021-10-01", end="2021-10-01")
-google = yf.download("GOOGL", start="2021-10-01", end="2021-10-01")
-
-data1 = company1.history(period="3mo")
-data2  = company2.history(period="3mo")
-
-st.write(
-
-)
-
-st.write(company1.info['longBusinessSummary'])
-st.write(apple)
-
-st.line_chart(data1.values)
-
-st.write(
-
-)
-st.write(company2.info['longBusinessSummary'], "\n", google)
-st.line_chart(data2.values)
+df
