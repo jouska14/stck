ed46492b6840ee657dbcf0048dc66f3ba5ac50e3
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-15 16:20:55 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -5,5 +5,12 @@ import datetime
 import numpy as np
 
 st.title('Stock Predictor')
-st.write( "Created By Nehal , Sara And Alok") 
 
+st.markdown('''
+# Stock Price App
+Shown are the stock price data for query companies!
+**Credits**
+- App built by Nehal, Sara, Alok
+- Built in `Python` using `streamlit`,`yfinance`, `pandas` and `datetime`
+''')
+st.write(---)
