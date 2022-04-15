1e0b7adeba6f64018e7edc569658168231f4aa23
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-15 16:13:26 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -1,16 +1,7 @@
 import streamlit as st
 import yfinance as yf
-
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
-import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-  'first column': ['nehal', 'kopal', 'molly', 'mishthi'],
-  'second column': [10, 20, 30, 40]
-})
+import datetime
+import numpy as np
 
-df
+st.title('Stock Predictor')
