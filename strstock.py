00aeec333791990a341d1a4cb286d5fc8b84d4ed
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-15 16:26:54 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -7,10 +7,12 @@ import numpy as np
 st.title('Stock Predictor')
 
 st.markdown('''
-# Stock Price App
+          # Stock Price App
 Shown are the stock price data for query companies!
 **Credits**
 - App built by Nehal, Sara, Alok
 - Built in `Python` using `streamlit`,`yfinance`, `pandas` and `datetime`
 ''')
 st.write('---')
+
+
