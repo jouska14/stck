d1a422de5ec5579eb5dbf220903d2fbe54fd6784
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-23 20:43:00 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -19,7 +19,7 @@ st.write('---')
 # Sidebar
 st.sidebar.subheader('Choose Your Query Parameter ')
 
-start_date = st.sidebar.date_input("Start Date", datetime.date.strptime('%Y-%m-%d'))
+start_date = st.sidebar.date_input("Start Date", datetime.('%Y-%m-%d'))
 end_date = datetime.today().strptime('%Y-%m-%d')
 
 stocks = ('AAPL','GOOGL', 'MSFT')
