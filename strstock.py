398e4bb9e25f79d8b1293f152258f52d00e7ec27
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-25 21:14:34 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -67,7 +67,7 @@ def create_dataset(dataset, time_step = 1):
         a = dataset[i:(i+time_step),0]
         dataX.append(a)
         dataY.append(dataset[i+time_step, 0]
-     return np.array(dataX), np.array(dataY)
+    return np.array(dataX), np.array(dataY)
                      
 time_step = 100
 x_train, y_train = create_dataset(train_data, time_step)
@@ -77,6 +77,7 @@ x_test, y_test = create_dataset(test_data, time_step)
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
 st.write(x_train)
+                     
 #Create the LSTM model
 
                      
