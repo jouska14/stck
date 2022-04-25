2e120897a8d270d3caae65e6f995e80be9d29380
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-25 21:37:37 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -85,8 +85,38 @@ model.add(LSTM(50, return_sequences= True))
 model.add(LSTM(50))
 model.add(Dense(1))
 model.compile(loss = 'mean_squared_error', optimizer = 'adam')
-st.write(model.summary())    
-          
+model.summary()   
+
+model.fit(X train,y_train, validation_data=(X_test,ytest), epochs=100, batch_size=64, verbose=1)
+
+#Lets predict and check performance metrics
+train_predict = model.predict(x_train)
+test_predict = model.predict(x_test)
+
+#Transform back to original form
+train_predict = scaler.inverse_transform(train_predict)
+test_predict = scaler.inverse_transform(test_predict)
+
+#Calculate RMSE performance metrics
+math.sqrt(mean_squared_error(y_train, train_predict))
+#Plotting
+
+#Shift train prediction for plotting
+look_back = 100
+trainPredictPlot = np.empty_like(df_close)
+trainPredictPlot[:,:] = np.nan
+trainPredictPlot[look_back:len(train_predict) + look_back, :] = train_predict
+
+#Shift test prediction for plotting
+testPredictPlot = np.empty_like(df_close)
+testPredictPlot[:,:] = np.nan
+testPredictPlot[len(train_predict) + (look_back * 2)+1:len(df_close) - 1, :] = test_predict
+
+#Plot baseline and predictions
+st.pyplot(scaler.inverse_transform(df_close))
+st.pyplot(trainPredictPlot)
+st.pyplot(testPredictPlot)
+
                      
                      
                      
