fff5f4e5b37987c7ac8b922bf6296c8399f3ab1a
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-28 21:44:49 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -30,30 +30,22 @@ end_date = datetime.date.today().strftime("%Y-%m-%d")
 
 stocks = ('AAPL','GOOGL', 'MSFT')
 tickerSymbol = st.sidebar.selectbox('Select',stocks)
-
-@st.cache
-def load_data(ticker):
-  data = yf.download(ticker,start_date,end_date)
-  data.reset_index(inplace=True)
-  return data
-
-data_load_state = st.text("Load data...")
-data = load_data(tickerSymbol)
-data_load_state.text("Loading data.... Done!")
+tickerData = yf.Ticker(tickerSymbol) # Get ticker data
+tickerDf = tickerData.history(period='1d', start=start_date, end=end_date) 
 
 # Ticker information
-string_logo = '<img src=%s>' % data.info['logo_url']
+string_logo = '<img src=%s>' % tickerData.info['logo_url']
 st.markdown(string_logo, unsafe_allow_html=True)
 
-string_name = data.info['longName']
+string_name = tickerData.info['longName']
 st.header('**%s**' % string_name)
 
-string_summary = data.info['longBusinessSummary']
+string_summary = tickerData.info['longBusinessSummary']
 st.info(string_summary)
 
 
-st.subheader('Raw Data')
-st.write(data)
+st.subheader('Ticker Data')
+st.write(tickerDf)
 
 def plot_raw_data():
     fig = go.Figure()
