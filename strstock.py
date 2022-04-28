46c47040de0d70cb48e62f750522a8337a806a3c
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-28 13:36:49 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -124,10 +124,9 @@ tepp = pd.DataFrame(testPredictPlot)
 st.write(tepp)
 st.write('This is how we split the Data')
 fig = px.line(data_close ,x='Date' , y=itdc[0], labels={'x':'Year' , 'y':'Closing price'})
-fig.add_trace(data_close, x=data['Date'],  y=tpp[0], mode='lines')
+fig.add_trace(data_close, y=tpp[0], mode='lines')
+fig.add_trace(data_close,, y=tepp[0], mode='lines')
 fig.show()
-fig2 = px.line(data_close,x= data['Date'], y=tepp[0])
-st.write(fig2)
 
 
 
