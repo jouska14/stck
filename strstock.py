5704e8f577db81f23fcde97f1b3dfe2859ce5f79
jouska14 <75094227+jouska14@users.noreply.github.com>
2022-04-28 18:02:46 +0530

    Update strstock.py

--- strstock.py
+++ strstock.py
@@ -167,8 +167,11 @@ day_pred=np.arange(1407,1467)
 st.write(day_new)
 st.write(day_pred)
 
+
 dp1 = scaler.inverse_transform(data_close[5768:])
 dp2 = scaler.inverse_transform(lst_output)
+st.write(dp1)
+st.write(dp2) 
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x= day_new, y=dp1, mode='lines'))
